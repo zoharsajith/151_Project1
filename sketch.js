d6642a1eb84e375fbbff8e5a84341453687ff704
Zohar Sajith <zoharsajith@gmail.com>
2020-09-16 04:08:56 -0500

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,22 +1,84 @@
-let x = 0; 
-let speed = 3;
+
+p5.disableFriendlyErrors = false;
+
+var angle = 2.0;
+var scalar = 3.5;
+var speed = 0.1;
+var bool = false;
+
+let s = [];
+
+var c = {
+	r:0,
+	g:0,
+	b:0
+};
+
+var m = {
+	mX:0,
+	mY:0
+}
 
 function setup() {
-  createCanvas(400, 400);
-  // put setup code here
+  createCanvas(windowWidth, windowHeight);
+  background(0);
+
 }
+
 
 function draw() {
-	background(100, 100, 100);
-	fill(20,30,100);
-	rect(x, 100, 100, 100);
 
-	if (x + 100 > width){
-		speed = (-1*speed);
-	} else if (x <= 0){
-		speed = 3;
+	for(let i = 0; i < s.length; i++){
+		s[i].draw();
+	}
+	
+}
+
+class spiral{
+	
+	draw(){
+		if(bool){
+		var offsetX = m.mX;
+		var offsetY = m.mY;
+		c.r = random(0, 255);
+		c.g = random(0, 100);
+		c.b = random(0, 100);
+
+
+	    var x = offsetX + cos(angle) * scalar;
+		var y = offsetY + sin(angle) * scalar;
+		fill(c.r, c.g, c.b);
+		noStroke();
+		ellipse(x, y, 5, 5);
+		angle += speed;
+		scalar += speed;
+		
+		}
 	}
 
-	x = x + speed;
-  // put drawing code here
 }
+
+
+
+function keyTyped(){
+		if(key === '1'){
+			saveCanvas('myCanvas', 'png');
+		}
+		return;
+	}
+
+ function mousePressed(){
+ 	bool = true;
+ 	m.mX = mouseX;
+	m.mY = mouseY;
+	angle = 2.0;
+	scalar = 3.5;
+
+	for(let i = 0; i < 10; i++){
+		s.push(new spiral());
+	}
+
+ 	
+ }
+
+
